56ad67273487a4299d2f4b66bee0d79422304db7
Saidfatah <said_designer@outlook.com>
2020-06-23 19:39:41 +0100

    fixed some bugs

--- flatPicker.js
+++ flatPicker.js
@@ -11,16 +11,22 @@ let currentDay      = new Date().getDate()
 let currentYear     =  2020
 let selectedDate    = ''
 let selectedDateRef =null
+let prevSelectedDateRefId =''
 const currentMonthFixed    = new Date().getMonth() 
 let prevMotnhDaysCount     = 0
 let currrentMotnhDaysCount = 0
 
 //lower level functions
-const createDay         = (day,month,OptionalClass)=> {
+const createDay         = (day,month,id,OptionalClass)=> {
     const date=monthNames[month] +' '+ day + ','+currentYear
-    return `<span class="flatPicker__day  ${OptionalClass +' '+( selectedDate === date?'selected':'') } " area_labe="${date}">${day}</span>`
+    return `<span 
+               id="${id}"
+               class="flatPicker__day  ${OptionalClass +' '+( selectedDate === date?'selected':'') } 
+               " area_labe="${date}">
+            ${day}
+            </span>`
 }
-const appendDays        =(days)=> days.forEach(day => {daysContainer.innerHTML += day})
+const appendDays        = (days)=> days.forEach(day => {daysContainer.innerHTML += day})
 const isLeapYear        = ()=>(currentYear % 100 === 0) ? (currentYear % 400 === 0) : (currentYear % 4 === 0)
 const monthNumberOfDays = ()=>{
     if (currentMonth == 0 || currentMonth == 2 || currentMonth == 4 || currentMonth == 6 || currentMonth == 7 || currentMonth == 9 || currentMonth == 11)
@@ -29,13 +35,21 @@ const monthNumberOfDays = ()=>{
        return isLeapYear() ? 29 : 28
     return 30
 }
+const stringBettwen     =(left ,ofset,right,str) => str.substring(str.lastIndexOf(left) + ofset,  str.lastIndexOf(right))
+const elem              =selector => document.querySelector(selector)
 
 //get the days
  const currrentMotnhDays = ()=>{
     let days = []
     currrentMotnhDaysCount = monthNumberOfDays()
     for (let index = 0; index < currrentMotnhDaysCount ; index++) {
-        const day = createDay(index +1,currentMonth,(currentDay==index+1 && currentMonthFixed == currentMonth)?'today':'')
+        const day = createDay(index +1,currentMonth,'c'+(index +1),(currentDay==index+1 && currentMonthFixed == currentMonth)?'today':'')
+        var createdDate = stringBettwen('area_labe="',11,'"',day) //this might return June 29,2020 for example 
+        log(createdDate == selectedDate)
+        if(createdDate == selectedDate){
+            prevSelectedDateRefId= 'c'+(index +1)
+            log(prevSelectedDateRefId)
+        }
         days.push(day)
     }
     return days
@@ -46,7 +60,7 @@ const monthNumberOfDays = ()=>{
     const day_of_week_where_currentMonth_starts = parseInt(new Date(currentYear, currentMonth, 1).getDay());
     prevMotnhDaysCount =  day_of_week_where_currentMonth_starts 
     for (let index = prev_month_lenght - day_of_week_where_currentMonth_starts ; index < prev_month_lenght ; index++) {
-      const day = createDay(index+1,currentMonth-1,'prev_next_Day')
+      const day = createDay(index+1,currentMonth-1,'p'+(index ),'prev_next_Day')
       days.push(day)
     } 
     return days
@@ -58,7 +72,7 @@ const monthNumberOfDays = ()=>{
     const next_Month_left_over_days = 42-(currrentMotnhDaysCount + prevMotnhDaysCount)
     let   days = []
     for (let index = 0; index < next_Month_left_over_days ; index++) {
-      const day = createDay(index + 1,currentMonth+1,'prev_next_Day')
+      const day = createDay(index + 1,currentMonth+1,'n'+(index ),'prev_next_Day')
       days.push(day)
     } 
     return days
@@ -101,10 +115,13 @@ const toSelectedMonth=e=>{
 const pickDate=e=>{
     if(e.target.classList.contains('flatPicker__day') )
     {
+        if(prevSelectedDateRefId != '') selectedDateRef=elem('#'+prevSelectedDateRefId)
+        prevSelectedDateRefId =''
+        
         selectedDate =e.target.getAttribute('area_labe')
-
-        if(selectedDateRef != null) selectedDateRef.classList.remove('selected')
+        if(selectedDateRef != null)selectedDateRef.classList.remove('selected')
         selectedDateRef    = e.target
+
         e.target.classList.add('selected')
   
     } 
